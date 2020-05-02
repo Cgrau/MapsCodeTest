47216b1c09874b48fcdafa83eab74eedf75a3abe
Carles Grau <carles.grau@ubeeqo.com>
2020-05-02 10:49:40 +0200

    Move HexColor to extensions

--- MapsCodeTestTests/Utils/XCTestCase+UIViewController.swift
+++ MapsCodeTestTests/Utils/XCTestCase+UIViewController.swift
@@ -4,8 +4,4 @@ extension XCTestCase {
   var viewControllerFrame: CGRect {
     return CGRect(origin: .zero, size: CGSize(width: 375, height: 667))
   }
-  
-  var navigationBarFrame: CGRect {
-    return CGRect(origin: .zero, size: CGSize(width: 750, height: 48))
-  }
 }
